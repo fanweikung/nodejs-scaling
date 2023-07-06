61c409b2ee2540851f450c80226d2cb17719dcd6
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-06 07:15:04 -0700

    Loadtest http server for each of the CPUs

--- ch01/01_04/start/index.js
+++ ch01/01_04/start/index.js
@@ -1,11 +1,18 @@
+const http = require("http");
 const cluster = require("cluster");
 const numCpus = require("os").cpus().length;
 
 if (cluster.isMaster) {
   console.log(`this is the master process: ${process.pid}`);
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
 } else {
-  console.log(`this is the worker process: ${process.pid}`);
+  http
+    .createServer((req, res) => {
+      let message = `worker ${process.pid}...`;
+      console.log(message);
+      res.end(message);
+    })
+    .listen(3000);
 }
