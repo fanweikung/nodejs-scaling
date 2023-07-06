cd6ff3c47b771cfa06fc3bb04fc2afabb0d61071
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-06 06:24:50 -0700

    Imported cluster module to fork processes

--- ch01/01_04/start/index.js
+++ ch01/01_04/start/index.js
@@ -1,1 +1,11 @@
+const cluster = require("cluster");
+const numCpus = require("os").cpus().length;
 
+if (cluster.isMaster) {
+  console.log(`this is the master process: ${process.pid}`);
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log(`this is the worker process: ${process.pid}`);
+}
